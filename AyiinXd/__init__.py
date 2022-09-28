6ef5dd6d4f41e95f92c0ed39bda48a55c9f2a66f
Ananda Chisato <114603132+tokonandapedia@users.noreply.github.com>
2022-09-29 05:35:36 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -101,7 +101,7 @@ DEVS = [
     1663258664,
     5146829516,
     777969420,
-        ]
+]
 
 SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
 BL_CHAT = {int(x) for x in os.environ.get("BL_CHAT", "").split()}
