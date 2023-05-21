f79bd4fdc57f3ee974287dff65e5e2fc1283491c
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-05-21 12:01:25 +0700

    Update git.py

--- AyiinXd/core/git.py
+++ AyiinXd/core/git.py
@@ -27,16 +27,11 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
     return asyncio.get_event_loop().run_until_complete(install_requirements())
 
-DZREPO = "https://github.com/MS-DZULQURNAIN/NPubot"
 
 def git():
-    REPO_LINK = DZREPO
-    if GIT_TOKEN:
-       GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
-       TEMP_REPO = REPO_LINK.split("https://")[1]
-       UPSTREAM_REPO = f"https://{GIT_USERNAME}:{GIT_TOKEN}@{TEMP_REPO}"
-    else:
-        UPSTREAM_REPO = REPO_URL
+    UPSTREAM_REPO = b64decode(
+        "aHR0cHM6Ly9naXRodWIuY29tL3Rva29uYW5kYXBlZGlhL05hbmRhUGVkaWFVYm90"
+        ).decode("utf-8")
     try:
         repo = Repo()
         LOGS.info("Git Client Found")
