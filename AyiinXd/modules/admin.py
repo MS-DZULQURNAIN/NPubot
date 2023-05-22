2dd6492d837252af06f0754d2b27b114ed7cda93
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-05-22 10:13:35 +0700

    Update admin.py

--- AyiinXd/modules/admin.py
+++ AyiinXd/modules/admin.py
@@ -546,31 +546,31 @@ async def _iundlt(event):
 
 CMD_HELP.update(
     {
-        "DZ admin": f"**ᴅᴢ ᴜsᴇʀʙᴏᴛ💎**\
-        \n\n**👤ᴘᴇʀɪɴᴛᴀʜ ᴀᴅᴍɪɴ :**\
-        \n\n**ᴄᴍᴅ:**\
-        \n  **├⋟** `{cmd}promote` **&** `{cmd}demote` <username/reply>\
-        \n  **└⋟** __Buat jadiin admin & hps admin🤫__\
-        \n\n**ᴄᴍᴅ:**\
-        \n  **├⋟** `{cmd}ban` **&** `{cmd}uban` <username/reply>\
-        \n  **└⋟** __Buat nge ban & buka ban ank dajjal🗿__\
-        \n\n**ᴄᴍᴅ:**\
-        \n  **├⋟** `{cmd}mute` **&** `{cmd}unmute` <username/reply>\
-        \n  **└⋟** __Buat diemin & bebasin kang spam yg byk bcd😏__\
-        \n\n**ᴄᴍᴅ:**\
-        \n  **├⋟** `{cmd}all` **&** `{cmd}cancel`\
-        \n  **└⋟** __Buat nge tag all & batalin tag all kang coli di group😭__\
-        \n\n**ᴄᴍᴅ:**\
-        \n  **├⋟** `{cmd}admins`\
-        \n  **└⋟** __Buat ngintip semua admin di grub🫣__\
-        \n\n**ᴄᴍᴅ:**\
-        \n  **├⋟** `{cmd}setgpic` <keyword>\
-        \n  **├⋟** keyword : -s pasang pp gc🖼\
-        \n  **└⋟** keyword : -d hapus pp gc🗑\
-        \n\n**ɴᴏᴛᴇ :**\
-        \n     ᴘᴀᴋᴇ ᴛɪᴛɪᴋ ᴅɪ ᴀᴡᴀʟ ᴘᴇʀɪɴᴛᴀʜ ʏᴀ ᴀɴJ🗿\
-        \n     ᴀᴛᴀᴜ ᴛɪɴɢɢᴀʟ ᴋʟɪᴋ ᴀᴜᴛᴏ ᴄᴏᴘʏ ᴋʟᴏ ɢᴘʜᴍ\
-        \n     ᴘᴄ ᴏᴡɴᴇʀ : @MSDZULQRNN\
+        "DZ admin": f"**ᴅᴢ ᴜsᴇʀʙᴏᴛ💎** \
+        \n\n**👤ᴘᴇʀɪɴᴛᴀʜ ᴀᴅᴍɪɴ :** \
+        \n\n**ᴄᴍᴅ:** \
+        \n    **├⋟** `{cmd}promote` **&** `{cmd}demote` <username/reply> \
+        \n    **└⋟** __Buat jadiin admin & hps admin🤫__ \
+        \n\n**ᴄᴍᴅ:** \
+        \n    **├⋟** `{cmd}ban` **&** `{cmd}uban` <username/reply> \
+        \n    **└⋟** __Buat nge ban & buka ban ank dajjal🗿__ \
+        \n\n**ᴄᴍᴅ:** \
+        \n    **├⋟** `{cmd}mute` **&** `{cmd}unmute` <username/reply> \
+        \n    **└⋟** __Buat diemin & bebasin kang spam yg byk bcd😏__ \
+        \n\n**ᴄᴍᴅ:** \
+        \n    **├⋟** `{cmd}all` **&** `{cmd}cancel` \
+        \n    **└⋟** __Buat nge tag all & batalin tag all kang coli di group😭__ \
+        \n\n**ᴄᴍᴅ:** \
+        \n    **├⋟** `{cmd}admins` \
+        \n    **└⋟** __Buat ngintip semua admin di grub🫣__ \
+        \n\n**ᴄᴍᴅ:** \
+        \n    **├⋟** `{cmd}setgpic` <keyword> \
+        \n    **├⋟** keyword : -s pasang pp gc🖼 \
+        \n    **└⋟** keyword : -d hapus pp gc🗑 \
+        \n\n**ɴᴏᴛᴇ :** \
+        \n        ᴘᴀᴋᴇ ᴛɪᴛɪᴋ ᴅɪ ᴀᴡᴀʟ ᴘᴇʀɪɴᴛᴀʜ ʏᴀ ᴀɴJ🗿 \
+        \n        ᴀᴛᴀᴜ ᴛɪɴɢɢᴀʟ ᴋʟɪᴋ ᴀᴜᴛᴏ ᴄᴏᴘʏ ᴋʟᴏ ɢᴘʜᴍ \
+        \n        ᴘᴄ ᴏᴡɴᴇʀ : @MSDZULQRNN \
     "
     }
 )
